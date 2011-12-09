c5ec28101570e221f618dca40e1ff9a0c3242b49
Brian Mehrman <bcmehrman@gmail.com>
2011-12-09 14:14:55 -0600

    bug fixin

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "donotreply@eatme.inventiongarden.com"
+  default from: "support@eatme.inventiongarden.com"
 
   def welcome_email(user)
     @user = user
